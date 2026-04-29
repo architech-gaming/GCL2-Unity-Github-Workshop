68fe48f9aeb92904a0c8d2511287e32fbcbfc9f4
Shan <architech.gaming@gmail.com>
2026-04-29 10:46:27 +0800

    [Shawn] Corrected spike script naming

--- Assets/Scripts/ShawnSpike.cs
+++ Assets/Scripts/ShawnSpike.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ShawnSpike : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
