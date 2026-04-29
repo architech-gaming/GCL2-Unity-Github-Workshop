6f1136a63eec2bceb931506411d211374a3357f3
Watermelon <qiuwenxi456@gmail.com>
2026-04-29 10:44:11 +0800

    Qiu Wenxi added purple unit, WenxiController script and yellow unit GameObject

---
+++ Assets/Scripts/WenxiController.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class WenxiController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("poop stupid");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
