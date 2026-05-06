eaf5d8bbb2e46962383bc642c250109a84e7ac63
Watermelon <qiuwenxi456@gmail.com>
2026-05-06 10:41:43 +0800

    wenxi changed debug message of WenxiController.cs to "poopy pants"

--- Assets/Scripts/WenxiController.cs
+++ Assets/Scripts/WenxiController.cs
@@ -6,7 +6,7 @@ public class WenxiController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("poop stupid");
+        Debug.Log("poopy pants");
     }
 
     // Update is called once per frame
